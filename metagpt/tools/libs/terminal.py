556954c8bac4aaef7348ee21b0626de0d22e398c
yswang0927 <wangys0927@gmail.com>
2025-11-20 20:51:22 +0800

    Add Windows Terminal support to terminal.py #1897

--- metagpt/tools/libs/terminal.py
+++ metagpt/tools/libs/terminal.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import re
+import sys
 from asyncio import Queue
 from asyncio.subprocess import PIPE, STDOUT
 from typing import Optional
@@ -22,8 +23,20 @@ class Terminal:
     """
 
     def __init__(self):
-        self.shell_command = ["bash"]  # FIXME: should consider windows support later
-        self.command_terminator = "\n"
+        #self.shell_command = ["bash"]  # FIXME: should consider windows support later
+        #self.command_terminator = "\n"
+        # Add Windows Terminal support to terminal.py #1897
+        if sys.platform.startswith("win"):
+            self.shell_command = ["cmd.exe"]  # Windows
+            self.executable = None
+            self.command_terminator = "\r\n"
+            self.pwd_command = "cd"
+        else:
+            self.shell_command = ["bash"]  # Linux / macOS
+            self.executable = "bash"
+            self.command_terminator = "\n"
+            self.pwd_command = "pwd"
+
         self.stdout_queue = Queue(maxsize=1000)
         self.observer = TerminalReporter()
         self.process: Optional[asyncio.subprocess.Process] = None
@@ -33,6 +46,9 @@ class Terminal:
             # serve cmd have a space behind it,
             "serve ": "Use Deployer.deploy_to_public instead.",
         }
+        # yswang add
+        self.chat_id = ""
+        self.role = None;
 
     async def _start_process(self):
         # Start a persistent shell process
@@ -41,9 +57,10 @@ class Terminal:
             stdin=PIPE,
             stdout=PIPE,
             stderr=STDOUT,
-            executable="bash",
+            executable=self.executable,
             env=os.environ.copy(),
-            cwd=DEFAULT_WORKSPACE_ROOT.absolute(),
+            #cwd=DEFAULT_WORKSPACE_ROOT.absolute(),
+            cwd=str(DEFAULT_WORKSPACE_ROOT) if sys.platform.startswith("win") else DEFAULT_WORKSPACE_ROOT,  # Windows
         )
         await self._check_state()
 
@@ -51,7 +68,8 @@ class Terminal:
         """
         Check the state of the terminal, e.g. the current directory of the terminal process. Useful for agent to understand.
         """
-        output = await self.run_command("pwd")
+        #output = await self.run_command("pwd")
+        output = await self.run_command(self.pwd_command)
         logger.info("The terminal is at:", output)
 
     async def run_command(self, cmd: str, daemon=False) -> str:
@@ -74,6 +92,8 @@ class Terminal:
         output = ""
         # Remove forbidden commands
         commands = re.split(r"\s*&&\s*", cmd)
+
+        """
         for cmd_name, reason in self.forbidden_commands.items():
             # "true" is a pass command in linux terminal.
             for index, command in enumerate(commands):
@@ -87,7 +107,23 @@ class Terminal:
         self.process.stdin.write(
             f'echo "{END_MARKER_VALUE}"{self.command_terminator}'.encode()  # write EOF
         )  # Unique marker to signal command end
+        """
+        skip_cmd = "echo Skipped" if sys.platform.startswith("win") else "true"
+        for cmd_name, reason in self.forbidden_commands.items():
+            # "true" is a pass command in linux terminal.
+            for index, command in enumerate(commands):
+                if cmd_name in command:
+                    output += f"Failed to execute {command}. {reason}\n"
+                    commands[index] = skip_cmd
+        cmd = " && ".join(commands)
+        # Send the command
+        self.process.stdin.write((cmd + self.command_terminator).encode())
+
+        marker_cmd = f"echo {END_MARKER_VALUE}"
+        self.process.stdin.write((marker_cmd + self.command_terminator).encode())  # Unique marker to signal command end
+
         await self.process.stdin.drain()
+
         if daemon:
             asyncio.create_task(self._read_and_process_output(cmd))
         else:
@@ -116,7 +152,10 @@ class Terminal:
             This function wraps `run_command`, prepending the necessary Conda activation commands
             to ensure the specified environment is active for the command's execution.
         """
-        cmd = f"conda run -n {env} {cmd}"
+        #cmd = f"conda run -n {env} {cmd}"
+        # windows & linux conda run
+        cmd = f"conda activate {env} && {cmd}" if sys.platform.startswith("win") else f"conda run -n {env} {cmd}"
+
         return await self.run_command(cmd, daemon=daemon)
 
     async def get_stdout_output(self) -> str:
@@ -147,10 +186,10 @@ class Terminal:
                     continue
                 *lines, tmp = output.splitlines(True)
                 for line in lines:
-                    line = line.decode()
+                    line = line.decode(errors="ignore")
                     ix = line.rfind(END_MARKER_VALUE)
                     if ix >= 0:
-                        line = line[0:ix]
+                        line = line[:ix]
                         if line:
                             await observer.async_report(line, "output")
                             # report stdout in real-time
@@ -164,8 +203,21 @@ class Terminal:
 
     async def close(self):
         """Close the persistent shell process."""
-        self.process.stdin.close()
-        await self.process.wait()
+        if self.process:
+            self.process.stdin.close()
+            await self.process.wait()
+
+    # yswang add
+    def set_chat_id(self, chat_id: str):
+        self.chat_id = chat_id
+        self.observer.set_chat_id(chat_id)
+
+    def get_chat_id(self) -> str:
+        return self.chat_id
+
+    def set_role(self, role):
+        self.observer.set_role(role)
+
 
 
 @register_tool(include_functions=["run"])
